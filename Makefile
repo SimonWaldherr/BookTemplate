d56ecb7ae14bfb324802d1d69529178e4c48db74
Simon Waldherr <git@simon.waldherr.eu>
2025-04-09 17:57:39 +0200

    v1.1.0

--- Makefile
+++ Makefile
@@ -1,3 +1,19 @@
-book:
-	quarto render;
+# Makefile for rendering the Quarto project and additional formats
 
+.PHONY: all render djvu clean
+
+# Default target renders the book and then converts to DJVU.
+all: render djvu
+
+# Render the Quarto book.
+render:
+	quarto render
+
+# Convert the generated PDF to DJVU format.
+# Requires the "pdf2djvu" command to be available.
+djvu:
+	pdf2djvu _book/Book-Title.pdf -o _book/Book-Title.djvu
+
+# Clean up generated artifacts.
+clean:
+	rm -rf _book *.log *.aux *.toc *.out
--- default.nix
+++ default.nix
@@ -1,24 +1,34 @@
-let
- pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/976fa3369d722e76f37c77493d99829540d43845.tar.gz") {};
- rpkgs = builtins.attrValues {
-  inherit (pkgs.rPackages) quarto;
-};
-  tex = (pkgs.texlive.combine {
-  inherit (pkgs.texlive) scheme-small amsmath framed fvextra environ fontawesome5 orcidlink pdfcol tcolorbox tikzfill;
-});
- system_packages = builtins.attrValues {
-  inherit (pkgs) R glibcLocalesUtf8 quarto;
-};
-  in
-  pkgs.mkShell {
-    LOCALE_ARCHIVE = if pkgs.system == "x86_64-linux" then  "${pkgs.glibcLocalesUtf8}/lib/locale/locale-archive" else "";
-    LANG = "en_US.UTF-8";
-    LC_ALL = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
+# Default Nix configuration for the Quarto and LaTeX project
 
-    buildInputs = [  rpkgs tex system_packages  ];
-      
-  }
+let
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/976fa3369d722e76f37c77493d99829540d43845.tar.gz") {};
+
+  # R packages used in the project
+  rpkgs = builtins.attrValues {
+    inherit (pkgs.rPackages) quarto;
+  };
+
+  # TeX Live packages required for PDF rendering
+  tex = pkgs.texlive.combine {
+    inherit (pkgs.texlive)
+      scheme-small amsmath framed fvextra environ fontawesome5 orcidlink pdfcol tcolorbox tikzfill;
+  };
+
+  # Additional system packages now include pdf2djvu for PDF-to-DJVU conversion.
+  system_packages = builtins.attrValues {
+    inherit (pkgs) R glibcLocalesUtf8 quarto pdf2djvu;
+  };
+
+in pkgs.mkShell {
+  # Set locale for consistent output
+  LOCALE_ARCHIVE = if pkgs.system == "x86_64-linux" then "${pkgs.glibcLocalesUtf8}/lib/locale/locale-archive" else "";
+  LANG = "en_US.UTF-8";
+  LC_ALL = "en_US.UTF-8";
+
+  buildInputs = rpkgs ++ [
+      tex
+      pkgs.R
+      pkgs.quarto
+      pkgs.pdf2djvu
+    ];
+}
